294379645c59b347f0c3e9311d41cef3ffeda697
Romango <homer37@yandex.ru>
2022-09-22 06:46:12 +0400

    first one

--- controllers/api-post-controller.js
+++ controllers/api-post-controller.js
@@ -1,16 +1,18 @@
-const Post = require('../models/post');
+const Post = require("../models/post");
 
 const handleError = (res, error) => {
   res.status(500).send(error.message);
-}
+};
 
 const getPosts = (req, res) => {
-  Post
-    .find()
-    .sort({ createdAt: -1 })
-    .then((posts) => res.status(200).json(posts))
-    .catch((error) => handleError(res, error));
-}
+  const posts = [
+    { id: 1, name: "oneName" },
+    { id: 3, name: "oneName" },
+    { id: 4, name: "oneName" },
+  ];
+  const newPosts = JSON.stringify(posts)
+  res.status(200).send(newPosts);
+};
 
 const addPost = (req, res) => {
   const { title, author, text } = req.body;
@@ -19,36 +21,33 @@ const addPost = (req, res) => {
     .save()
     .then((post) => res.status(200).json(post))
     .catch((error) => handleError(res, error));
-}
+};
 
 const getPost = (req, res) => {
-  Post
-    .findById(req.params.id)
+  Post.findById(req.params.id)
     .then((post) => res.status(200).json(post))
     .catch((error) => handleError(res, error));
-}
+};
 
 const deletePost = (req, res) => {
   const { id } = req.params;
-  Post
-  .findByIdAndDelete(id)
-  .then((post) => res.status(200).json(id))
-  .catch((error) => handleError(res, error));
-}
+  Post.findByIdAndDelete(id)
+    .then((post) => res.status(200).json(id))
+    .catch((error) => handleError(res, error));
+};
 
 const editPost = (req, res) => {
   const { title, author, text } = req.body;
   const { id } = req.params;
-  Post
-    .findByIdAndUpdate(id, { title, author, text }, { new: true })
+  Post.findByIdAndUpdate(id, { title, author, text }, { new: true })
     .then((post) => res.json(post))
     .catch((error) => handleError(res, error));
-}
+};
 
 module.exports = {
   getPosts,
   addPost,
-  getPost, 
+  getPost,
   deletePost,
   editPost,
 };
